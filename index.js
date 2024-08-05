acd8f4f5b8a0942eafe679b367e8b978e2989b5e
ronishstha <stharonish@gmail.com>
2024-08-05 20:01:16 +0545

    Add route for handling code verification

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
+app.use(cors());
+app.use(express.json());
+
+const INPUT_SIZE = 6;
+
 app.get("/", (req, res) => {
-  res.send("Initialization");
+  res.send("Initializad!");
+});
+
+app.post("/verify", (req, res) => {
+  console.log("res", req.body);
+  const { code } = req.body;
+  if (code.slice(-1) === "7" || code.length !== INPUT_SIZE) {
+    res.status(400).json({
+      success: false,
+      message: "Verification Error!",
+    });
+  }
+  res.status(200).json({
+    success: true,
+    message: "Code verified successfully!",
+  });
 });
 
 app.listen(port, () => {

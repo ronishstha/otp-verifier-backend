0ab0475cfbb926fa454ee9eca1344b7bbea2e362
ronishstha <stharonish@gmail.com>
2024-08-05 20:59:23 +0545

    Add .env for FRONTEND_URL

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
 const app = express();
 const port = 3000;
 
-app.use(cors());
+dotenv.config();
+
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const INPUT_SIZE = 6;
